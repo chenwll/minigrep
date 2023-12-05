388acf7a3f7558d7e0d6075049c10f77cbbb1a01
ChenWL <1963383112@qq.com>
2023-12-05 20:17:35 +0800

    增加模块化和错误处理

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,22 @@
 // in main.rs
-use std::env;
-use std::fs;
-
+use minigrep::Config;
+use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
-
-    let content = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     // cargo run -- test sample.txt
     // 在sample.txt寻找test字符
     println!("Search for {}", config.query);
     println!("In file {}", config.file_path);
-}
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Config {
-            query,
-            file_path,
-        }
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process.exit(1);
     }
 }
+
